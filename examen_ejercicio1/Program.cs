0bf4be636c85dd0bd82983f353f3504077c8a63d
Ivan Reyes <ivinreyes1@gmail.com>
2021-02-22 11:06:03 -0600

    prueba

---
+++ examen_ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace examen_ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 0;
+            double price = 0;
+            Console.WriteLine("Ingrese el precio del articulo");
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de articulo");
+            amount = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Tota: $" + price * amount);
+        }
+    }
+}
